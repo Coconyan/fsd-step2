9697df98d2eb55ebfa5d6a7e02e5c6287070a9ce
Coconyan <San3k19@gmail.com>
2020-06-13 22:50:29 +0700

    Test HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { template } = require('lodash');
 
 module.exports = {
-   mode: 'development',
-   entry: './src/index.js',
-   output: {      
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'main.js',
-   },
+  mode: 'development',
+  entry: {
+    main: './src/index.js'
+  },
+  output: {      
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
 };
