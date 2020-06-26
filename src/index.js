1ea9864cbb104e0d153a3bf43c70cb975e4ab5da
Coconyan <San3k19@gmail.com>
2020-06-26 23:08:45 +0700

    add input-text

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ import './assets/scss/nullstyle.scss';
 import './assets/scss/main.scss';
 import './assets/scss/colors.scss';
 import './assets/scss/fonts.scss';
+import './assets/scss/form-elements.scss';
